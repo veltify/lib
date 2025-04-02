054dde2e60d4f29247c3c8cef0a1e88492432135
TheHadiAhmadi <thehadiahmadi@gmail.com>
2025-04-02 22:15:00 +0430

    update

--- src/lib/cms/components/Navigation.svelte
+++ src/lib/cms/components/Navigation.svelte
@@ -2,16 +2,14 @@
 	import NavItem from "./NavItem.svelte";
 	import NavMenu from "./NavMenu.svelte";
 
-	import { page } from "$app/state";
-
-	let { links } = $props();
+	let { pathname, links } = $props();
 </script>
 
 <NavMenu>
-	<NavItem active={page.url.pathname == "/admin"} name="Home" href="/admin" />
+	<NavItem active={pathname == "/admin"} name="Home" href="/admin" />
 	{#each Object.keys(links) as slug}
 		<NavItem
-			active={page.url.pathname.startsWith("/admin/" + slug)}
+			active={pathname.startsWith("/admin/" + slug)}
 			name={links[slug]}
 			href="/admin/{slug}"
 		/>
